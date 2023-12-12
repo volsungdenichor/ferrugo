7ace1c58ab4539c56ad76da351f0c5dab69978ae
krzysiek <krzysztof.muhleisen@gmail.com>
2023-12-12 19:55:04 +0100

    pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <ferrugo/iterator_interface.hpp>
 #include <ferrugo/operators.hpp>
 #include <ferrugo/optional.hpp>
+#include <ferrugo/pipeline.hpp>
 #include <ferrugo/range_ref.hpp>
 #include <ferrugo/std_ostream.hpp>
 #include <ferrugo/type_name.hpp>
@@ -14,17 +15,17 @@
 #include <tuple>
 #include <vector>
 
-ferrugo::iterable<int> create_vec()
-{
-    return std::vector<int>{ 2, 5, 9 };
-}
-
 void run()
 {
-    for (auto x : create_vec())
-    {
-        std::cout << x << std::endl;
-    }
+    using ferrugo::fn;
+    std::vector<int> vect = { 1, 2, 3, 5 };
+    const auto f = 10                                  //
+                   | fn([](int x) { return x + 1; })   //
+                   | fn([](int x) { return 9 * x; })   //
+                   | fn([](int x) { return x - 98; })  //
+                   | fn([&](int x) -> const int& { return vect[x]; });
+
+    std::cout << f << std::endl;
 }
 
 int main(int argc, char const* argv[])
